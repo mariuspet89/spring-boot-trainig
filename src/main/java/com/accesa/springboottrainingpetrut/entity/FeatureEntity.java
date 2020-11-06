3e03235abbfebf88dda126d469867afcf8fef546
marius.petrut <marius.petrut@accesa.eu>
2020-11-06 15:54:54 +0200

    New feature enetity added

---
+++ src/main/java/com/accesa/springboottrainingpetrut/entity/FeatureEntity.java
@@ -0,0 +1,14 @@
+package com.accesa.springboottrainingpetrut.entity;
+
+public class FeatureEntity {
+
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
