70503a0b4de410d66bcbb233b375022a2722bf27
marius.petrut <marius.petrut@accesa.eu>
2020-11-06 15:14:55 +0200

    rezolvare erroare -classpath:/ db.migration

--- src/main/java/com/accesa/springboottrainingpetrut/controller/InternController.java
+++ src/main/java/com/accesa/springboottrainingpetrut/controller/InternController.java
@@ -1,7 +1,6 @@
 package com.accesa.springboottrainingpetrut.controller;
 
 import com.accesa.springboottrainingpetrut.dto.InternDto;
-import com.accesa.springboottrainingpetrut.entity.Intern;
 import com.accesa.springboottrainingpetrut.service.InternService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,49 +13,10 @@ import java.util.List;
 import java.util.Optional;
 
 
-/*
 @RestController
 @RequestMapping("/interns")
 @OpenAPIDefinition
 @Tag(name="Interns")
-public class InternController {
-
-    private final InternService internService;
-
-
-    public InternController(InternService internService) {this.internService = internService; }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public InternDto save(@RequestBody InternDto internDto) {return internService.save(internDto);}
-
-    @GetMapping
-    public List<InternDto> findInterns() {return internService.findAll();}
-
-    @GetMapping("/{id}")
-    public InternDto findById(@PathVariable Long id){
-        Optional<InternDto> internDto = internService.findById(id);
-        return internDto.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
-    public InternDto findByName(@RequestParam(value = "name") String name) {
-        Optional<InternDto> internDto = internService.findByName(name);
-        return  internDto.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
-    @PutMapping
-    public  InternDto update(@RequestBody InternDto internDto) {return internService.save(internDto);}
-
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {internService.deleteById(id);}
-
-
-}
-*/
-@RestController
-@RequestMapping("/interns")
-@OpenAPIDefinition
-@Tag(name = "Interns")
 public class InternController {
 
     private final InternService internService;
--- src/main/resources/db/migration/V2_0__populate_intern_table.sql
+++ src/main/resources/db/migration/V2_0__populate_intern_table.sql
@@ -3,7 +3,6 @@ insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Andrei Tigan'
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Cristi (Dan) Flore', 'dan.flore@accesa.eu', '2004-12-31');
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Dan Goia', 'dan.goia@accesa.eu', '2004-12-31');
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Doru Varga', 'doru.varga@accesa.eu', '2004-12-31');
-
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Marius Petrut', 'marius.petrut@accesa.eu', '2004-12-31');
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Oana Pantea', 'oana.pantea@accesa.eu', '2004-12-31');
 insert into INTERNS values(nextval('PUBLIC', 'INTERNS_SEQUENCE'), 'Raluca Gabor', 'raluca.gabor@acces.eu', '2004-12-31');
